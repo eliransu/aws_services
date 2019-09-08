a0d054bc14f515d4c870fd447512a4198fa9b43a
eliransu <elikos1@gmail.com>
2019-09-08 22:35:25 +0300

    .

--- web-service/index.js
+++ web-service/index.js
@@ -7,11 +7,11 @@ app.get('/ping', (req, res) => {
     res.json('pong');
 })
 
-app.listen(8080, error => {
+app.listen(80, error => {
     if (error) {
         console.error("Faild to run server: " + error);
         throw error;
     } else {
-        console.log("Server is running on port 8080");
+        console.log("Server is running on port 80");
     }
 })
